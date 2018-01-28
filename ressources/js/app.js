671a580228abc939b9b16fbea75ba4a3984148e7
pebriniel <zeldalyoko@gmail.com>
2018-01-28 12:15:13 +0100

    ajout initial

---
+++ ressources/js/app.js
@@ -0,0 +1,171 @@
+var gstats = {
+    playSound: true //on active-désactive le son
+};
+
+var game = new Array({
+   mouseX : 0,
+   mouseY : 0,
+
+   paused : false,
+   click : false
+});
+
+var sound = new Array(
+    {key: "sword", source: "sword.mp3"},
+    {key: "caverne", source: "caverne.mp3"},
+    {key: "voix-texte-1", source: "caverne.mp3"}
+);
+
+var event = new Array(
+
+    {type: 'SPRITE', class:"character-one", sprite: "charactere-move", posx: 0, posy: 150},
+    {type: 'MOVE', class:"character-one", sprite: "charactere-move", posx: 0, posx_obj: 150, posy: 150, vitesse: 6},
+
+    {type: 'TEXT', soundplay: "sword", soundrepeat: false, sprite: "maya-question", spriteStop: "maya-question-stop", text: "Bonjour ? Qui Ãªtes-vous ?", option: "fondu"},
+
+    {type: 'SOUND', key: "caverne", soundrepeat: false },
+
+
+    {type: 'TEXT', sprite: "maya-reflexion", spriteStop: "maya-reflexion-stop", text: "Pouvez-vous m'aider ?"},
+    {type: 'TEXT', sprite: "maya-reflexion-stop", spriteStop: "maya-reflexion-stop", text: "..."},
+    {type: 'TEXT', sprite: "maya-reflexion", spriteStop: "maya-reflexion-stop", text: "Je cherche le directeur, on m'a dit qu'il s'appelait Boutade"},
+    {type: 'TEXT', sprite: "maya-reflexion-stop", spriteStop: "maya-reflexion-stop", text: "..."},
+    {type: 'TEXT', sprite: "maya-reflexion", spriteStop: "maya-excitation-stop", text: "..."},
+    {type: 'TEXT', sprite: "maya-question", spriteStop: "maya-question-stop", text: "Vous aussi vous Ãªtes ici pour les inscriptions de l'Access Code School ?!"},
+    {type: 'TEXT', sprite: "maya-excitation", spriteStop: "maya-excitation-stop", text: "Je suis tellement exitée de m'inscrire, pas vous ?"},
+    {type: 'TEXT', sprite: "maya-triste", spriteStop: "maya-triste-stop", text: "Il n'y plus beaucoup de place, j'espère que l'on sera pris"}
+);
+
+var script = new Array(
+
+);
+
+var i = 0;
+var intervalText = false;
+function lireText(chaine = "coucou"){
+   var nbCaracere = chaine.length;
+   var textbox = document.getElementsByClassName("tbox-content")[0];
+   var content = "";
+
+   content = chaine.substring(0, i);
+   textbox.innerHTML = content;
+   i ++;
+
+   if(i == (nbCaracere + 1)){
+       i = 0;
+       clearInterval(intervalText);
+       intervalText = false;
+   }
+}
+
+function MoveSprite(e = new Array(), sprite = ""){
+    if(e.posx_obj && sprite.offsetLeft < e.posx_obj){
+        console.log(sprite.style.offsetLeft);
+        sprite.style.left = (sprite.offsetLeft + e.vitesse)+'px';
+    }
+}
+
+var e_actuel = -1;
+var intervalEvent;
+var action_joueur = 0;
+var sprite;
+function lectureEvent(){
+   var caractere = document.getElementsByClassName("caractere")[0];
+   var screen = document.getElementById("screen1");
+
+   if(e_actuel < (event.length - 1)){
+       if(!intervalText && action_joueur != e_actuel){
+           e_actuel = action_joueur;
+
+           //si l'event actuel est supÃ©rieur Ã  0 on supprime la prÃ©cÃ©dente div
+           if(e_actuel > 0){
+               caractere.classList.remove("fondu");
+               caractere.classList.remove(event[e_actuel - 1].spriteStop);
+               caractere.classList.remove(event[e_actuel - 1].sprite);
+           }
+
+           //changement de la sprite dans la div
+           if(event[e_actuel].option){
+               console.log('fondu');
+               caractere.className = "fondu caractere";
+           }
+
+           sprite = event[e_actuel].sprite;
+           if(!caractere.classList.contains(sprite)){
+               caractere.classList.add(sprite);
+           }
+
+           if(event[e_actuel].background){
+               console.log('dev');
+               screen.className = event[e_actuel].background;
+           }
+
+           //chaine de caractere dites par le personnage
+           if(event[e_actuel].type == 'TEXT'){
+               chaine = event[e_actuel].text;
+               if(event[e_actuel].soundplay){
+                   playSound(event[e_actuel].soundplay, event[e_actuel].soundrepeat);
+               }
+               intervalText = setInterval((function(){lireText(chaine); }), 100); //on joue l'interval de texte qiu dois balancer le bordel
+           }
+           else if(event[e_actuel].type == 'SOUND'){
+                s_key = event[e_actuel].key;
+                playSound(s_key, event[e_actuel].soundrepeat);
+           }
+           else if(event[e_actuel].type == 'SPRITE'){
+               var sprite = document.getElementsByClassName(event[e_actuel].class)[0];
+               sprite.style.top = event[e_actuel].posy;
+               sprite.style.left = event[e_actuel].posx;
+
+               if(event[e_actuel].sprite){
+                   sprite.className = event[e_actuel].sprite;
+               }
+           }
+           else if(event[e_actuel].type == 'MOVE'){
+                   var sprite = document.getElementsByClassName(event[e_actuel].class)[0];
+                   sprite.style.top = event[e_actuel].posy;
+                   sprite.style.left = event[e_actuel].posx;
+
+                intervalMove = setInterval((function(){MoveSprite(event[e_actuel], sprite); }), 300); //on joue l'interval de texte qiu dois balancer le bordel
+           }
+       }
+       else{
+           //e_actuel ++;
+           if(!intervalText && caractere.classList.contains(sprite)){
+               caractere.classList.remove(sprite);
+               sprite2 = event[e_actuel].spriteStop;
+               caractere.classList.add(sprite2);
+               game.click = false;
+           }
+       }
+   }
+   else{
+       clearInterval(intervalEvent);
+   }
+}
+
+$.ready(document, (function(){
+   intervalEvent = setInterval(lectureEvent, 300);
+
+
+   for(var s = 0; s < sound.length; s ++){
+       loadSound("ressources/audio/"+sound[s].source, sound[s].key);
+   }
+
+   $.click(".button-next", (function(){
+       if(!game.click){
+           console.log("ok");
+           action_joueur ++;
+           game.click = true;
+       }
+   }));
+
+   $.mousemove("#screen2", (function(e){
+       var screen2 = document.querySelectorAll("#screen2")[0];
+       var game = document.querySelectorAll("#game")[0];
+
+       game.mouseX = (e.clientX - game.offsetLeft);     // Get the horizontal coordinate
+       game.mouseY = (e.clientY - game.offsetTop) - 190;
+   }));
+
+}));
---
+++ ressources/js/sound.js
@@ -0,0 +1,55 @@
+var sounds = new Array();
+var soundSource = new Array();
+
+// Fix up prefixing
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var context = new AudioContext();
+
+function onError(){ console.log('erreur on');  }
+
+function loadSound(file, key) {
+	var request = new XMLHttpRequest();
+	request.open('GET', file, true);
+	request.responseType = 'arraybuffer';
+
+	console.log(file);
+
+	// Decode asynchronously
+	request.onload = function() {
+		context.decodeAudioData(request.response, function(buffer) {
+			sounds[key] = buffer;
+		}, (function(){
+				console.log('erreur 2');
+
+			}));
+	}
+
+	request.send();
+}
+
+function playSound(key, loop){
+	if(gstats.playSound){
+		var source = context.createBufferSource(); // creates a sound source
+		source.buffer = sounds[key];                    // tell the source which sound to play
+		source.connect(context.destination);
+		source.loop = loop;
+		source.start(0);                           // play the source now
+														// note: on older systems, may have to use deprecated noteOn(time);
+		if(loop){
+			soundSource[key] = source;
+		}
+	}
+}
+
+function stopSound(key, all){
+	if(gstats.playSound){
+		if(all === 'undefined'){
+			soundSource[key].stop();
+		}
+		else{
+			for(i in soundSource){
+				soundSource[i].stop();
+			}
+		}
+	}
+}
